18c2609b7f9e7ab170250596084b03ad21ce9e20
Xavier Marques <xavierpmarques@gmail.com>
2021-06-24 23:49:09 +0100

    import code from other .ts files instead of having most code on index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,11 @@
-// todoModule
-const todoModule = (() => {
+require ('./style.scss');
 
-type Priority = 'low' | 'medium' | 'high';
+import { todoModule } from "./todo";
+import { projectModule } from "./project";
+import { dom } from "./domGenerator";
 
-class Todo {
-    parentProject: string;
-    priority: Priority;
-    title: string;
-    description: string;
-    dueDate: Date;
-    notes: string[];
-    completed: boolean;
-    
-    constructor(title: string, priority: Priority, description?: string) {
-        this.title = title;
-        this.priority = priority;
-        this.description = description;
-        // this.dueDate = dueDate;
-        this.notes = null;
-        this.completed = false;
-    }
-
-    changeTitle (newTitle: string) {
-        this.title = newTitle;
-    }
-
-    changePriority (newPriority: Priority) {
-        this.priority = newPriority;
-    }
-
-    changeDescription (newDescription: string) {
-        this.description = newDescription;
-    }
-    
-    changeDueDate (newDueDate: Date) {
-        this.dueDate = newDueDate;
-    }
-
-    addNewNote (noteToAdd: string) {
-        this.notes.push(noteToAdd);
-    }
-
-    toggleCompleteStatus() {
-        this.completed = !this.completed;
-    }
-}
-
-const newTodo = (title: string, priority: Priority, description?: string) => {
-    let newTodoObject = new Todo(title, priority, description);
-    return newTodoObject;
-}
-
-return {
-    newTodo: newTodo,
-}
-
-})();
+dom.generateHeader();
 
 
-const projectModule = (() => {
 
-class Project {
-    title: string;
-    description: string;
-    children: object[];
-    
-    constructor(title: string, description?: string) {
-        this.title = title;
-        this.description = description;
-        this.children = [];
-    }
-}
 
-const newProject = (title: string, description?: string) => {
-    let newObject = new Project(title, description);
-    return newObject;
-}
-
-const addTodoToProject = (todo: object, project: Project) => {
-    project.children.push(todo);
-}
-
-return {
-    newProject: newProject,
-    addTodoToProject : addTodoToProject
-}
-
-})();
