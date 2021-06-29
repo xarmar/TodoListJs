06ad426ca92e1056e767386dff769750dc4a60d0
Xavier Marques <xavierpmarques@gmail.com>
2021-06-29 23:01:43 +0100

    add removeChildNodes() and isBlank() helper functions

--- src/helperFunctions.ts
+++ src/helperFunctions.ts
@@ -3,3 +3,13 @@ export const appendMultipleNodesToParent = (parentNode, ...childNodes) => {
         parentNode.append(child);
     });
 }
+
+export const removeChildNodes = (parentNode) => {
+    while(parentNode.firstChild) {
+        parentNode.removeChild(parentNode.firstChild);
+    }
+}
+
+export const isBlank = ch => {
+	return ch.match(/^\s*$/i) !== null;
+}
