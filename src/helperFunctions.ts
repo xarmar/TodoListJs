ff7f633d0baa43335a7b311bec799312d1e72c42
Xavier Marques <xavierpmarques@gmail.com>
2021-06-30 00:29:47 +0100

    add currentDate() function.

--- src/helperFunctions.ts
+++ src/helperFunctions.ts
@@ -13,3 +13,12 @@ export const removeChildNodes = (parentNode) => {
 export const isBlank = ch => {
 	return ch.match(/^\s*$/i) !== null;
 }
+
+export const currentDate = () => {
+    let currentDate = new Date();
+    let cYear = currentDate.getFullYear();
+    let cMonth = currentDate.getMonth() + 1;
+    let cDay = currentDate.getDate();
+    let today:string = cYear + '-' + cMonth + '-' + cDay ;
+    return today;
+}
