0d6e08a3fba2c59124ae9d744934f51e735109bd
Xavier Marques <xavierpmarques@gmail.com>
2021-06-30 20:08:38 +0100

    Add all functions inside a module

--- src/helperFunctions.ts
+++ src/helperFunctions.ts
@@ -1,41 +1,58 @@
-export const appendMultipleNodesToParent = (parentNode, ...childNodes) => {
-    childNodes.forEach(child => {
-        parentNode.append(child);
-    });
-}
+export const helperfunction = (() => {
 
-export const removeChildNodes = (parentNode) => {
-    while(parentNode.firstChild) {
-        parentNode.removeChild(parentNode.firstChild);
+    const appendMultipleNodesToParent = (parentNode, ...childNodes) => {
+        childNodes.forEach(child => {
+            parentNode.append(child);
+        });
     }
-}
 
-export const isBlank = ch => {
-	return ch.match(/^\s*$/i) !== null;
-}
+    const removeChildNodes = (parentNode) => {
+        while(parentNode.firstChild) {
+            parentNode.removeChild(parentNode.firstChild);
+        }
+    }
 
-export const currentDate = () => {
-    let currentDate = new Date();
-    let cYear = currentDate.getFullYear();
+    const isBlank = ch => {
+        return ch.match(/^\s*$/i) !== null;
+    }
+
     
-    let month = currentDate.getMonth() + 1;
-    let cMonth;
-    if (month < 10) {
-        cMonth = '0' + month;
-    }
-    else {
-        cMonth = month
+    const currentDate = () => {
+        let currentDate = new Date();
+        let cYear = currentDate.getFullYear();
+        
+        let month = currentDate.getMonth() + 1;
+        let cMonth;
+        if (month < 10) {
+            cMonth = '0' + month;
+        }
+        else {
+            cMonth = month
+        }
+
+        let cDay;
+        let day = currentDate.getDate();
+        if (day < 10) {
+            cDay = '0' + day;
+        }
+        else {
+            cDay = day
+        }
+        
+        let today:string = cYear + '-' + cMonth + '-' + cDay ;
+        return today;
     }
 
-    let cDay;
-    let day = currentDate.getDate();
-    if (day < 10) {
-        cDay = '0' + day;
+    return {
+        appendMultipleNodesToParent: appendMultipleNodesToParent,
+        removeChildNodes: removeChildNodes,
+        isBlank: isBlank,
+        currentDate: currentDate
     }
-    else {
-        cDay = day
-    }
-    
-    let today:string = cYear + '-' + cMonth + '-' + cDay ;
-    return today;
-}
+
+})();
+
+
+
+
+
