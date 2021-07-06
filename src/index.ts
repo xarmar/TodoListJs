a5b89aaf27a51241449da85e6632acd84f9f065d
Xavier Marques <xavierpmarques@gmail.com>
2021-07-07 00:27:50 +0100

    remove one of the default projects

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,26 @@
-require ('./style.scss');
+require('./styles.scss');
 import { todoModule } from "./todo";
 import { projectModule, listOfProjects } from "./project";
 import { domNavBar } from "./dom/domNavBar";
 import { domGrid } from "./dom/domGrid";
 
-let shopppingProject = projectModule.newProject('Shopping');
-listOfProjects.push(shopppingProject);
 
 let exampleDate = new Date;
 let oneMoreWeek = new Date;
+let moviesToWatch = 'Movies To Watch';
 oneMoreWeek.setDate(oneMoreWeek.getDate() + 6);
 
-let bananas = todoModule.newTodo('Bananas', "low", exampleDate, 'Shopping', 'Must be ripe and ready to eat.');
-projectModule.appendTodoToProject(bananas, 'Shopping');
-let strawberries = todoModule.newTodo('Strawberries', "medium", oneMoreWeek, 'Shopping', 'Needs to be biological.');
-projectModule.appendTodoToProject(strawberries, 'Shopping');
-let icreCream = todoModule.newTodo('Ice cream', "high", exampleDate, 'Shopping', 'Vanilla or Chocolate');
-projectModule.appendTodoToProject(icreCream, 'Shopping');
-'Movies I want to watch'
 let moviesProject = projectModule.newProject('Movies To Watch', );
 listOfProjects.push(moviesProject);
 
-
-let rambo = todoModule.newTodo('Rambo', "low", exampleDate, 'Movies To Watch', 'Love Stallonne. Must watch.');
-projectModule.appendTodoToProject(rambo, 'Movies To Watch');
-let terminator = todoModule.newTodo('Terminator', "high", oneMoreWeek, 'Movies To Watch', "Haven't watched yet");
-projectModule.appendTodoToProject(terminator, 'Movies To Watch');
-let theyLive = todoModule.newTodo('They Live', "medium", exampleDate, 'Movies To Watch', 'I just ran out of bubblegum :)');
-projectModule.appendTodoToProject(theyLive, 'Movies To Watch');
+let rambo = todoModule.newTodo('Rambo', "low", exampleDate, moviesToWatch, 'Love Stallone. Must watch.');
+projectModule.appendTodoToProject(rambo, moviesToWatch);
+let terminator = todoModule.newTodo('Terminator', "high", oneMoreWeek, moviesToWatch, "Haven't watched yet");
+projectModule.appendTodoToProject(terminator, moviesToWatch);
+let theyLive = todoModule.newTodo('They Live', "medium", exampleDate, moviesToWatch, 'I just ran out of bubblegum :)');
+projectModule.appendTodoToProject(theyLive, moviesToWatch);
+let theThing = todoModule.newTodo('The Thing', 'medium', exampleDate, moviesToWatch, "Love scary movies!" );
+projectModule.appendTodoToProject(theThing, moviesToWatch);
 
 domNavBar.generateNavBar();
 domGrid.generateGrid();
