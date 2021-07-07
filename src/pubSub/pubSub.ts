b951df262270ec61d85d9d0322d764a4afe05f74
Xavier Marques <xavierpmarques@gmail.com>
2021-07-07 12:50:42 +0100

    Run diffent functions when it's a 'edit project' request vs a 'new project' request. Add function editProject()

--- src/pubSub/pubSub.ts
+++ src/pubSub/pubSub.ts
@@ -1,7 +1,7 @@
 import PubSub from 'pubsub-js';
 import { Todo, todoModule } from "../todo";
 import { domGrid } from "../dom/domGrid";
-import { projectModule, listOfProjects } from "../project";
+import { projectModule, listOfProjects, Project } from "../project";
 
 
 // Handles forms submissions and creates Todos and projects based on form input
@@ -10,6 +10,7 @@ export const pubSubModule = (() => {
     const newTodoFormSubmission = 'newTodoFormSubmition';
     const editTodoFormSubmission = 'editTodoFormSubmission';
     const newProjectFormSubmission = 'newProjectFormSubmission';
+    const editProjectFormSubmission = 'editProjectFormSubmission';
 
     const submitFormInfo = (title: string, description: string, priority?: string, dueDate?: Date, 
         projectTitle?: string, editOrNew?: string, titleOfProjectToBeEdited?:string, todoToBeEditedTitle?: string) => {
@@ -29,7 +30,12 @@ export const pubSubModule = (() => {
 
         // If it's a projectForm, just publish title
         else {
-            PubSub.publish(newProjectFormSubmission, {title})
+            if (editOrNew === 'new') {
+                PubSub.publish(newProjectFormSubmission, {title})
+            }
+            else {
+                PubSub.publish(editProjectFormSubmission, {title, projectTitle})
+            }
         }
     };
 
@@ -73,6 +79,27 @@ export const pubSubModule = (() => {
         let newProject = projectModule.newProject(title);
         listOfProjects.push(newProject);
         domGrid.updateSideBarProjects();
+    });
+
+    const editProject = PubSub.subscribe(editProjectFormSubmission, function(newTodoForm, {title, projectTitle}) {
+       let newTitle = title;
+       let originalTitle = projectTitle;
+
+        // identify project to edit
+       let projectToEdit: Project = projectModule.getProjectByTitle(originalTitle);
+    
+        // make sure Todos of the projectToEdit change parentProject to newTitle
+       projectToEdit.children.forEach(todo => {
+           todo.parentProject = newTitle;
+       });
+
+        //change title of projectToEdit
+       projectToEdit.changeTitle(newTitle);
+       
+        // update sidebar and screen with new title
+       domGrid.updateSideBarProjects();
+       domGrid.populateRightGrid(undefined, newTitle);
+
     });
 
     return  {
