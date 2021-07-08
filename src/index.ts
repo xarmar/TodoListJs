db3af26eea2a0bd514b58cfd9b59183fd9198636
Xavier Marques <xavierpmarques@gmail.com>
2021-07-08 16:03:08 +0100

    Remove code that was used for testing. Add function that checks if localStorage is supported by browser. If it is, localStorage is restored.

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,15 @@
 require('./styles.scss');
-import { todoModule } from "./todo";
-import { projectModule, listOfProjects } from "./project";
+import { restoreLocalCompletedTodosList } from "./todo";
+import { restoreLocalListOfProjects } from "./project";
+import { storageAvailable } from "./localStorage";
 import { domNavBar } from "./dom/domNavBar";
 import { domGrid } from "./dom/domGrid";
 
-
-let exampleDate = new Date;
-let oneMoreWeek = new Date;
-let moviesToWatch = 'Movies To Watch';
-oneMoreWeek.setDate(oneMoreWeek.getDate() + 6);
-
-let moviesProject = projectModule.newProject('Movies To Watch', );
-listOfProjects.push(moviesProject);
-
-let rambo = todoModule.newTodo('Rambo', "low", exampleDate, moviesToWatch, 'Love Stallone. Must watch.');
-projectModule.appendTodoToProject(rambo, moviesToWatch);
-let terminator = todoModule.newTodo('Terminator', "high", oneMoreWeek, moviesToWatch, "Haven't watched yet");
-projectModule.appendTodoToProject(terminator, moviesToWatch);
-let theyLive = todoModule.newTodo('They Live', "medium", exampleDate, moviesToWatch, 'I just ran out of bubblegum :)');
-projectModule.appendTodoToProject(theyLive, moviesToWatch);
-let theThing = todoModule.newTodo('The Thing', 'medium', exampleDate, moviesToWatch, "Love scary movies!" );
-projectModule.appendTodoToProject(theThing, moviesToWatch);
+// If localStorage is supported by current Browser restore listOfProjects and completedTodosList
+if (storageAvailable('localStorage')) {
+    restoreLocalListOfProjects();
+    restoreLocalCompletedTodosList();
+  }
 
 domNavBar.generateNavBar();
 domGrid.generateGrid();
